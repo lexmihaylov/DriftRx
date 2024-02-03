0424d00574ddd763aebd7bdad6c7ebc882a5ac8a
Alexander Mihaylov <lex.mihaylov@gmail.com>
2024-02-03 23:28:41 +0200

    added jsdocs

--- lib/StreamHistory.ts
+++ lib/StreamHistory.ts
@@ -5,9 +5,26 @@ export type StreamStoreStapshot = {
     [actioName: string]: any;
 };
 
+/**
+ * Represents the history of a stream store, tracking changes and actions.
+ */
 export class StreamHistory {
+    /**
+     * Array to store snapshots of the stream store.
+     * @private
+     */
     private snapshots: StreamStoreStapshot[] = [];
+
+    /**
+     * Subscription object to listen for stream store events.
+     * @private
+     */
     private subscription: Subscription;
+
+    /**
+     * Creates an instance of StreamHistory.
+     * @param {StreamStore} streamStore - The stream store to track.
+     */
     constructor(private streamStore: StreamStore) {
         this.subscription = this.streamStore.storeEvents$.subscribe((event: StreamStoreEvent) => {
             const currentSnapshot = this.snapshots[this.snapshots.length - 1] || {};
@@ -28,22 +45,42 @@ export class StreamHistory {
         })
     }
 
+    /**
+     * Gets the length of the stream history (number of snapshots).
+     * @returns {number} The length of the stream history.
+     */
     getLength() {
         return this.snapshots.length;
     }
 
+    /**
+     * Gets the last snapshot in the stream history.
+     * @returns {StreamStoreSnapshot} The last snapshot.
+     */
     getLastSnapshot() {
         return this.snapshots[this.snapshots.length - 1];
     }
 
+    /**
+     * Gets a snapshot at a specific index in the stream history.
+     * @param {number} index - The index of the desired snapshot.
+     * @returns {StreamStoreSnapshot} The snapshot at the specified index.
+     */
     getSnapshotByIndex(index: number) {
         return this.snapshots[index];
     }
 
+    /**
+     * Gets the full stream history as an array of snapshots.
+     * @returns {StreamStoreSnapshot[]} The full stream history.
+     */
     getFullHistory() {
         return this.snapshots;
     }
 
+    /**
+     * Destroys the stream history by clearing snapshots and unsubscribing from events.
+     */
     destroy() {
         this.snapshots = [];
         this.subscription.unsubscribe();
